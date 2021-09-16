2bddf3574e85112a60b5fe302250549efbead0da
Richeir <315310840@qq.com>
2021-09-16 22:33:12 +0800

    [add] gorm support

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/richeir-devops/cryar-api
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.5.0 // indirect
+require (
+	github.com/labstack/echo/v4 v4.5.0 // indirect
+	gorm.io/driver/mysql v1.1.2 // indirect
+	gorm.io/gorm v1.21.15 // indirect
+)
--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type UserInfo struct {
+	ID       uint
+	Nickname string
+	Age      uint8
+}
 
 func main() {
 	fmt.Println("Hello Echo API")
@@ -20,6 +28,7 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/users", getUsers)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
@@ -28,4 +37,18 @@ func main() {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, Echo API!")
+}
+
+func getUsers(c echo.Context) error {
+	dsn := "root:richeir@tcp(127.0.0.1:3306)/cryar?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var user UserInfo
+	db.Take(&user, 1)
+
+	return c.String(http.StatusOK, user.Nickname)
 }
